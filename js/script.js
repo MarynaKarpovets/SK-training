e4fba58a576e26710b30737334ba6fdb33c9b624
MarynaKarpovets <garmonijasvitu@gmail.com>
2021-04-05 21:35:55 +0300

    Added training18

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// let str = "Lorem ipsum dolor sit amet";
+// let arr = str.split (' ');
+// function compareNum(a,b){
+// if(a > b) return 1;
+// if(a == b) return 0;
+// if(a < b) return -1;
+// }
+// let arr = [1,2,10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+
+// let arr = [1,2,3,4];
+// for (const iterator of arr) {
+//     console.log(iterator);
+// }
+
+
+
+// let arg = prompt("input your number", "0");
+
+// if (arg != null) {
+//    searchElem(arg);
+// } else {
+//     console.log("Cancel was pressed")
+// }
+
+// function searchElem(arg){
+//     let res = arr.indexOf(+arg);
+//     if (res == -1) {
+//         console.log("Num not found");
+//     } else {
+//         console.log(res);
+//     }
+// }
+
+let arr = ['html', 'css', 'javascript'];
+let nameLengths = arr.map((name) => name.length);
+console.log(nameLengths); 
+
+// arr.forEach(function(item, i, arr) {
+// alert( i + ": " + item + "(массив:" + arr + ")" );}):
+// }
+

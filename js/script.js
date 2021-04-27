03df1f9c5c5b817cf0b4aa2020b321d0445c82e3
MarynaKarpovets <garmonijasvitu@gmail.com>
2021-04-27 20:41:55 +0300

    Added training20

---
+++ js/script.js
@@ -0,0 +1,68 @@
+//Task 1//
+// let filter = function(a){
+//     return(a > 0 && a < 5)? true: false;
+// }
+
+// let a = 9;
+// console.log(filter(a));
+// if {
+// 	console.log(true);
+// } else {
+// 	console.log(false);
+// }
+
+// console.log((a > 0 && a < 5)? true: false); 
+
+//Task 2//
+// function print(str){
+//     console.log(str);
+//     // alert(str);
+// }
+
+// function getValidatedArg(num){
+//     if (typeof num != "number"){
+//         num = parseInt(num);
+//         print("Параметр має неправильный тип " + typeof num);
+//     }
+//     return num;
+// }
+// function getSelectedValue(num){
+//     let result;
+//     num = getValidatedArg(num);
+//         switch (num) {
+//             case 1:
+//               result = 'зима';
+//               break;
+//             case 2:
+//               result = 'весна';
+//               break;
+//             case 3:
+//               result = 'літо';
+//               break;
+//             case 4:
+//               result = 'осінь';
+//               break;
+//            default:
+//                result = "Я не знаю такого значення " + num;
+//         }
+//     return result;
+//     }
+    
+// let num = prompt("Уведіть число від 1 до 4");
+// print(getSelectedValue(num));
+
+//Task 3//
+//Дана строка 'aaa bbb ccc ".
+// Вырез с нее слово "bbb" тремя различными способами
+// (через substr, substring, slice).
+
+let str = 'aaa bbb ccc';
+let arg = 'bbb'
+
+
+
+function customSubStr(str, arg) {
+    src = ;
+    return result;
+  }
+  
